c299709e914025d160c768e71f0443b829d76643
Lejla1502 <lejlasp@hotmail.com>
2022-03-27 21:35:37 +0200

    adding UsersResponse

---
+++ EFCore_MySql_Example.WebApi/Responses/UsersResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCore_MySql_Example.WebApi.Responses
+{
+    public class UsersResponse:BaseResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
